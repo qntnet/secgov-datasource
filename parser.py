7e0d1cd7180bb98c90efef847605b75e98b81522
Stefan Munjas <stefan.munjas.tt@gmail.com>
2023-05-09 23:00:20 +0200

    Missing reports fix

--- parser.py
+++ parser.py
@@ -218,9 +218,13 @@ def extract_segment(ctx):
             ss.append({'type': 'explicit', 'dimension':s['attrib']['dimension'], 'value': v.strip()})
         elif s['tag'] == 'xbrldi:typedMember':
             dim = s['attrib']['dimension']
-            child = s['children'][0]
-            if child['attrib'].get('xsi:nil') == 'true':
+            try:
+                child = s['children'][0]
+                if child['attrib'].get('xsi:nil') == 'true':
+                    child = None
+            except:
                 child = None
+
             ss.append({'type': 'typed', 'dimension':dim, 'value': child})
         else:
             print(s)
